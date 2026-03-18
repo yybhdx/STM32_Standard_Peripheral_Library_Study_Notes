cc8728690a7075a7d441af2f07c9ac81827232e6
jifei <2750664768@qq.com>
2026-03-18 13:26:27 +0800

    code:add comments docs:update docs

--- 7-2 AD多通道(指定通道)(单次转换非扫描,滑动变阻器，对射红外，光敏电阻，热敏电阻传感器)/Objects/delay.d
+++ 7-2 AD多通道(指定通道)(单次转换非扫描,滑动变阻器，对射红外，光敏电阻，热敏电阻传感器)/Objects/delay.d
@@ -1,7 +1,7 @@
 .\objects\delay.o: System\Delay.c
 .\objects\delay.o: .\Start\stm32f10x.h
 .\objects\delay.o: .\Start\core_cm3.h
-.\objects\delay.o: D:\Software\Keil\keil_Core\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\delay.o: E:\keil MDK\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\delay.o: .\Start\system_stm32f10x.h
 .\objects\delay.o: .\User\stm32f10x_conf.h
 .\objects\delay.o: .\Library\stm32f10x_adc.h
